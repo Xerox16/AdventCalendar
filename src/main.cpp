3115a15e9d44b2d11f35efc3b1598b95a60bcb8e
Ert <ert.schneider@yahoo.de>
2015-11-01 00:26:05 +0100

    Fixed linker error in makefile and added snowfall (not yet usable)

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,59 @@
 
 #include <chrono>
 #include <iostream>
-
+#include <math.h>
 
 using std::cerr;
 using std::endl;
 
+SDL_Surface* display;
 
+#define PITCH (display->pitch / 4)
+
+void init()
+{
+  int i, j, pos;
+  for (i = 0; i < 640; i++)
+  {
+    int p = (int)((sin((i + 3247) * 0.02) * 0.3 + 
+                 sin((i + 2347) * 0.04) * 0.1 +
+             sin((i + 4378) * 0.01) * 0.6) * 100 + 380);
+    pos = p * PITCH + i;
+    for (j = p; j < 480; j++)
+    {
+      ((unsigned int*)display->pixels)[pos] = 0x007f00;
+      pos += PITCH;
+    }
+  }
+}
+
+void newsnow()
+{
+  int i;
+  for (i = 0; i < 8; i++)
+    ((unsigned int*)display->pixels)[rand() % 638 + 1] = 0xffffff;
+}
+
+void snowfall()
+{
+  int i, j;
+  unsigned int *fb = (unsigned int*)display->pixels;
+  for (j = 478; j >= 0; j--)
+  {
+    int ypos = j * PITCH;
+    for (i = 1; i < 639; i++)
+    {
+      if (fb[ypos + i] == 0xffffff)
+      {
+        if (fb[ypos + i + PITCH] == 0)
+        {
+          fb[ypos + i + PITCH] = 0xffffff;
+          fb[ypos + i] = 0;
+        }
+      }
+    }
+  }
+}
 
 
 int main()
@@ -22,8 +69,7 @@ int main()
  }
 
  // Set the video mode
- SDL_Surface* display;
- display = SDL_SetVideoMode(450, 500, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
+ display = SDL_SetVideoMode(1200, 805, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
  if (display == NULL)
  {
   cerr << "SDL_SetVideoMode() Failed: " << SDL_GetError() << endl;
@@ -31,12 +77,13 @@ int main()
  }
 
  // Set the title bar
- SDL_WM_SetCaption("SDL Tutorial", "SDL Tutorial");
+ SDL_WM_SetCaption("Adventskalendar", "Adventskalendar");
 
-// Load the image
- SDL_Surface* image;
- image = SDL_LoadBMP("media/background.bmp");
- if (image == NULL)
+// Load the background and the door
+ SDL_Surface* background,* door;
+ background = SDL_LoadBMP("media/kalender.bmp");
+ door       = SDL_LoadBMP("media/background.bmp");
+ if (background == NULL || door == NULL)
  {
      cerr << "SDL_LoadBMP() Failed: " << SDL_GetError() << endl;
      exit(1);
@@ -45,29 +92,22 @@ int main()
 // Load the sound file
   Mix_Music *music;
 	music = Mix_LoadMUS("media/music.mp3");
-
+ if (music == NULL)
+ {
+     cerr << "Mix_LoadMUS() Failed: " << SDL_GetError() << endl;
+     exit(1);
+ }
 
     //Initialize SDL_mixer 
-    if( Mix_OpenAudio( 22050, AUDIO_S16SYS, 2, 2048 ) == -1 ) {
-	return false;
-    } 
+    if( Mix_OpenAudio( 22050, AUDIO_S16SYS, 2, 2048 ) == -1 ) 
+ {
+     cerr << "Mix_OpenAudio() Failed: " << SDL_GetError() << endl;
+     exit(1);
+ }
     // Set Volume
     Mix_VolumeMusic(50);
-
-    if(music == NULL) {
-	std::cout<<"Could not load sound file!";
-	return -1;
-    }
-    
-    //If there is no music playing 
+    // Play Music
     Mix_PlayMusic( music, 1 );
-
-    int startTime = SDL_GetTicks();
-    // Wait
-    while (Mix_PlayingMusic()) {
-		SDL_Delay(1000);
-		std::cout << "Time: " << (SDL_GetTicks() - startTime) / 1000 << std::endl;
-	} 
 
  // Main loop
  SDL_Event event;
@@ -84,12 +124,30 @@ int main()
    }
   }
   // Game loop will go here...
-  // Apply the image to the display
-  if (SDL_BlitSurface(image, NULL, display, NULL) != 0)
+  // Apply the background to the display
+  if (SDL_BlitSurface(background, NULL, display, NULL) != 0)
   {
  cerr << "SDL_BlitSurface() Failed: " << SDL_GetError() << endl;
  exit(1);
   }
+
+    // Lock surface if needed
+  if (SDL_MUSTLOCK(display))
+    if (SDL_LockSurface(display) < 0) 
+      return -1;
+
+  // Ask SDL for the time in milliseconds
+  SDL_GetTicks();
+
+  newsnow();
+  snowfall();
+
+  // Unlock if needed
+  if (SDL_MUSTLOCK(display)) 
+    SDL_UnlockSurface(display);
+
+  // Tell SDL to update the whole display
+  SDL_UpdateRect(display, 0, 0, 1200, 805);   
 
   //Update the display
   SDL_Flip(display);
